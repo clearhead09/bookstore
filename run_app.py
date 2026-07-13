4ae2aa0dca52e13a6011c5bbc3bd400ac1d9423c
Clearhead09 <aiden0taylor@gmail.com>
2026-07-13 18:18:07 +1200

    Created run_app.py to run ngrok/Flask and setup environment for app.py

---
+++ run_app.py
@@ -0,0 +1,21 @@
+import os
+import threading
+from subprocess import call
+from pyngrok import ngrok
+
+def start_ngrok():
+    # Start ngrok tunnel
+    public_url = ngrok.connect(5001)
+    print(f"\n * Starting ngrok tunnel \"{public_url}\" -> \"http://")
+
+def open_app():
+    # Open the main app file to run Flask
+    call(["python", "app.py"])
+
+if __name__ == "__main__":
+    # Start ngrok in a separate thread
+    ngrok_thread = threading.Thread(target=start_ngrok, daemon=True)
+    ngrok_thread.start()
+
+    # Open the app
+    open_app()
